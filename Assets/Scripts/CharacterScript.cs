dea0dba02c50157ac9418b33a9460253ce96dd52
Jeff Dolan <jeffreydolan13@gmail.com>
2013-09-22 12:10:33 -0500

    Added more animations to player, wrote new movement script

---
+++ Assets/Scripts/CharacterScript.cs
@@ -0,0 +1,88 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum DIRECTIONS : int {
+	EAST = 0,
+	NORTH = 2,
+	WEST = 4,
+	SOUTH = 6
+}
+
+public enum ANIMATIONS : int {
+	IDLE = 0,
+	WALK = 1
+}
+
+public class CharacterScript : MonoBehaviour {
+	public int speed = 1000;
+
+	private int direction = 0;
+	private int anima = 0;
+	
+	private CharacterController controller;
+	private tk2dSpriteAnimator anim;
+
+	// Use this for initialization
+	void Start () {
+		controller = GetComponent<CharacterController>();
+		anim = GetComponent<tk2dSpriteAnimator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float x = Input.GetAxis("Horizontal") * speed;
+		float z = Input.GetAxis("Vertical") * speed;
+		Debug.Log(x + " " + z);
+		Vector3 movement = new Vector3(x, 0, z);
+		processInput(x, z);
+		playAnimation();
+		movement *= Time.deltaTime;
+		controller.Move(movement);
+	}
+	
+	private void playAnimation() {
+		if (anima == (int) ANIMATIONS.IDLE) {
+			if (!anim.IsPlaying("idle" + direction)) {
+				anim.Play("idle" + direction);
+			}
+		} else if (anima == (int) ANIMATIONS.WALK) {
+			
+		}
+	}
+	
+	private void processInput(float x, float z) {
+		//bool xIsZero = isZero(x);
+		//bool zIsZero = isZero(z);
+		bool xIsZero = x == 0;
+		bool zIsZero = z == 0;
+		if (xIsZero && zIsZero) {
+			anima = 0;
+		} else if (xIsZero) {
+			if (z > 0) {
+				direction = (int) DIRECTIONS.NORTH;
+			} else {
+				direction = (int) DIRECTIONS.SOUTH;
+			}
+			//anima = 1;
+		} else if (zIsZero) {
+			if (x > 0) {
+				direction = (int) DIRECTIONS.EAST;
+			} else {
+				direction = (int) DIRECTIONS.WEST;
+			}
+			//anima = 1;
+		} else {
+			
+		}
+	}
+	
+	/*private bool isZero(float num) {
+		if (num > 0.01) {
+			return false;
+		} else if (num < -0.01) {
+			return false;
+		} else {
+			return true;
+		}
+	}*/
+}
--- Assets/Scripts/Movement.js
+++ Assets/Scripts/Movement.js
@@ -10,7 +10,6 @@ function Update () {
   var x = Input.GetAxis("Horizontal") * Time.deltaTime * speed;
   var z = Input.GetAxis("Vertical") * Time.deltaTime * speed;
   var controller : CharacterController = GetComponent(CharacterController);
-  var y = -10 * Time.deltaTime; var movement : Vector3 = Vector3(x, y, z);
+  var movement : Vector3 = Vector3(x, 0, z);
   controller.Move(movement);
-  //transform.Translate(x, y, 0);
 }
