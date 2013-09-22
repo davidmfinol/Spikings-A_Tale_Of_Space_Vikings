f69eeff88ab7cdcdefc1a0b98a8daf676b3bebf9
Jeff Dolan <jeffreydolan13@gmail.com>
2013-09-22 09:28:43 -0500

    Added Animations

--- Assets/Scripts/Movement.js
+++ Assets/Scripts/Movement.js
@@ -8,6 +8,9 @@ function Start () {
 
 function Update () {
   var x = Input.GetAxis("Horizontal") * Time.deltaTime * speed;
-  var y = Input.GetAxis("Vertical") * Time.deltaTime * speed;
-  transform.Translate(x, y, 0);
+  var z = Input.GetAxis("Vertical") * Time.deltaTime * speed;
+  var controller : CharacterController = GetComponent(CharacterController);
+  var y = -10 * Time.deltaTime; var movement : Vector3 = Vector3(x, y, z);
+  controller.Move(movement);
+  //transform.Translate(x, y, 0);
 }
