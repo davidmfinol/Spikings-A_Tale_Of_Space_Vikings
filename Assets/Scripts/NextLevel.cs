066af95b1b6d9748581f168f3811c72df85d4c83
davidmfinol <davidmfinol@gmail.com>
2013-10-09 15:54:08 -0500

    Connecting 2 scenes together.

---
+++ Assets/Scripts/NextLevel.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NextLevel : MonoBehaviour {
+	
+	
+	void OnTriggerEnter (Collider collider) {
+		PlayerScript player = gameObject.GetComponent<PlayerScript>();
+		if(player != null)
+			Application.LoadLevel("EnemyTest");
+	}
+}
