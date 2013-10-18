2433b97aa6e1fbcc89407cfd2d689a24c61b37e2
davidmfinol <davidmfinol@gmail.com>
2013-10-18 13:57:59 -0500

    Max text increased.

--- Assets/Scripts/HelpScript.cs
+++ Assets/Scripts/HelpScript.cs
@@ -10,14 +10,11 @@ public class HelpScript : MonoBehaviour {
 			if (script == 0) {
 				GameManager.Instance.Hud.showThought("Move with WASD or Arrow Keys.");
 			} else if (script == 1) {
-				GameManager.Instance.Hud.showThought("If I had my hammer I could smash these.");
-				//I think my hammer flew to the east.
+				GameManager.Instance.Hud.showThought("If I had my hammer I could smash these. I think my hammer flew to the east.");
 			} else if (script == 2) {
-				GameManager.Instance.Hud.showThought("Attack with your hammer Q or F.");
-				//You can smash rocks with your hammer.
+				GameManager.Instance.Hud.showThought("Attack with your hammer Q or F. You can smash rocks with your hammer.");
 			} else if (script == 3) {
-				GameManager.Instance.Hud.showThought("E to push platform on dirt.");
-				//Space to jump on platform on dirt.
+				GameManager.Instance.Hud.showThought("E to push platform on dirt. Space to jump on platform on dirt.");
 			}
 			else
 				GameManager.Instance.Hud.hideThought();
