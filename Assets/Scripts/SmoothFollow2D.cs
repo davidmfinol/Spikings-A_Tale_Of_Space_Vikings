ae2ae2bc44355d8b8e5caaa59d0267c9c08d1c69
davidmfinol <davidmfinol@gmail.com>
2013-10-22 00:50:42 -0500

    Fixed up the tutorial level a bit

--- Assets/Scripts/SmoothFollow2D.cs
+++ Assets/Scripts/SmoothFollow2D.cs
@@ -5,12 +5,13 @@ public class SmoothFollow2D : MonoBehaviour {
 	
 	public Transform target;
 	public float smoothTime = 0.3f;
+	public float zOffset = 150;
 	private Vector3 velocity;
 	
 	void Update () {
 		Vector3 tmp = transform.position;
 		tmp.x = Mathf.SmoothDamp(transform.position.x, target.position.x, ref velocity.x, smoothTime);
-		tmp.z = Mathf.SmoothDamp(transform.position.z, target.position.z, ref velocity.z, smoothTime);
+		tmp.z = Mathf.SmoothDamp(transform.position.z, target.position.z + zOffset, ref velocity.z, smoothTime);
 		transform.position = tmp;
 	}
 }
