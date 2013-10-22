0eb448bb4aedc98ac4cdce8c5241b2ffd9cf0778
davidmfinol <davidmfinol@gmail.com>
2013-10-22 02:14:26 -0500

    Cleaned up the messages for the tutorial level

--- Assets/Scripts/HelpScript.cs
+++ Assets/Scripts/HelpScript.cs
@@ -3,12 +3,16 @@ using System.Collections;
 
 public class HelpScript : MonoBehaviour {
 	public string message;
+	private bool alreadyTriggered = false;
 	
 	void OnTriggerEnter(Collider collider) {
 		PlayerScript player = collider.gameObject.GetComponent<PlayerScript>();
 		if (player != null) {
-			if( message != null && message != "")
+			if( message != null && message != "" && !alreadyTriggered)
+			{
 				GameManager.Instance.Hud.showThought(message);
+				alreadyTriggered = true;
+			}
 			else
 				GameManager.Instance.Hud.hideThought();
 		}
