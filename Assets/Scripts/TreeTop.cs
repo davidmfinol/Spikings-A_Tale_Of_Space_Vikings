d9837c80f10a376bd9520a988ec05157152a24f3
davidmfinol <davidmfinol@gmail.com>
2013-10-22 01:20:47 -0500

    fixed up all known code bug on tutorial level except rock jumping

---
+++ Assets/Scripts/TreeTop.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TreeTop : TileScript {
+	
+	override protected void Start () {
+		Vector3 pos = transform.position;
+		pos.y = GameManager.Instance.MapData.height-((int)pos.z) / 128 + pos.y + 1;
+		transform.position = pos;
+	}
+}
